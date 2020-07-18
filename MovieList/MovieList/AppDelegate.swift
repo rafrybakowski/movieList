0b22a2cb6eb43ff3d9ac3ae538b73a1a260a3001
Rafał Rybakowski <rybakowski.r@gmail.com>
2020-07-18 11:19:51 +0200

    initial commit

---
+++ MovieList/MovieList/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MovieList
+//
+//  Created by Rafał Rybakowski on 18/07/2020.
+//  Copyright © 2020 Rafał Rybakowski. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
